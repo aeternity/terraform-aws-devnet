1e7c9f9aab2f62ef748c65479cbe6e0e01aea1b3
Ivaylo Papazov <papazov@vmandco.com>
2019-10-03 15:26:39 +0300

    Add DNS A records for next environment

--- main.tf
+++ main.tf
@@ -96,3 +96,11 @@ module "aws_deploy-next-eu-west-2" {
     aws = "aws.eu-west-2"
   }
 }
+
+resource "aws_route53_record" "next" {
+  zone_id = "${var.dns_zone}"
+  name    = "${var.next_domain}"
+  type    = "A"
+  records = ${modules.aws_deploy-next-eu-west-2.static_node_ips}
+  ttl     = 60
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,11 @@ variable "bootstrap_version" {
 variable "package" {
   default = "https://s3.eu-central-1.amazonaws.com/aeternity-node-builds/aeternity-latest-ubuntu-x86_64.tar.gz"
 }
+
+variable "dns_zone" {
+  default = "ZSEEAAX46MKWZ"
+}
+
+variable "next_domain" {
+  default = "next"
+}
