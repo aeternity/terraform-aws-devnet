6927deb86cf47c16c49036ba26fd85b5190ae04b
Ivaylo Papazov <papazov@vmandco.com>
2020-02-04 15:49:57 +0200

    Disable dev1,2 environments

--- main.tf
+++ main.tf
@@ -1,48 +1,48 @@
-module "aws_deploy-dev1-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
-  env               = "dev1"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
+# module "aws_deploy-dev1-eu-west-2" {
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+#   env               = "dev1"
+#   bootstrap_version = var.bootstrap_version
+#   vault_role        = "ae-node"
+#   vault_addr        = var.vault_addr
 
-  spot_nodes_min = 3
-  spot_nodes_max = 3
+#   spot_nodes_min = 3
+#   spot_nodes_max = 3
 
-  spot_price    = "0.07"
-  instance_type = "m4.large"
-  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+#   spot_price    = "0.07"
+#   instance_type = "m4.large"
+#   ami_name      = "aeternity-ubuntu-16.04-v1549009934"
 
-  aeternity = {
-    package = var.package
-  }
+#   aeternity = {
+#     package = var.package
+#   }
 
-  providers = {
-    aws = "aws.eu-west-2"
-  }
-}
+#   providers = {
+#     aws = "aws.eu-west-2"
+#   }
+# }
 
-module "aws_deploy-dev2-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
-  env               = "dev2"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
+# module "aws_deploy-dev2-eu-west-2" {
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+#   env               = "dev2"
+#   bootstrap_version = var.bootstrap_version
+#   vault_role        = "ae-node"
+#   vault_addr        = var.vault_addr
 
-  spot_nodes_min = 2
-  spot_nodes_max = 2
+#   spot_nodes_min = 2
+#   spot_nodes_max = 2
 
-  spot_price    = "0.07"
-  instance_type = "m4.large"
-  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+#   spot_price    = "0.07"
+#   instance_type = "m4.large"
+#   ami_name      = "aeternity-ubuntu-16.04-v1549009934"
 
-  aeternity = {
-    package = var.package
-  }
+#   aeternity = {
+#     package = var.package
+#   }
 
-  providers = {
-    aws = "aws.eu-west-2"
-  }
-}
+#   providers = {
+#     aws = "aws.eu-west-2"
+#   }
+# }
 
 module "aws_deploy-integration-eu-west-2" {
   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
