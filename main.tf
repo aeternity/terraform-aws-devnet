c7c7336dc9d32213930e349b6aef1f3b6e1d15d4
Dincho Todorov <dincho@users.noreply.github.com>
2021-07-26 16:50:03 +0300

    Update TF modules and AMI (18.04)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # module "aws_deploy-dev1-eu-west-2" {
-#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.0"
 #   env               = "dev1"
 #   bootstrap_version = var.bootstrap_version
 #   vault_role        = "ae-node"
@@ -10,19 +10,15 @@
 
 #   spot_price    = "0.07"
 #   instance_type = "m4.large"
-#   ami_name      = "aeternity-ubuntu-16.04-v1549009934"
-
-#   aeternity = {
-#     package = var.package
-#   }
+#   ami_name      = "aeternity-ubuntu-18.04-v1616681631"
 
 #   providers = {
-#     aws = "aws.eu-west-2"
+#     aws = aws.eu-west-2
 #   }
 # }
 
 # module "aws_deploy-dev2-eu-west-2" {
-#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.0"
 #   env               = "dev2"
 #   bootstrap_version = var.bootstrap_version
 #   vault_role        = "ae-node"
@@ -33,19 +29,15 @@
 
 #   spot_price    = "0.07"
 #   instance_type = "m4.large"
-#   ami_name      = "aeternity-ubuntu-16.04-v1549009934"
-
-#   aeternity = {
-#     package = var.package
-#   }
+#   ami_name      = "aeternity-ubuntu-18.04-v1616681631"
 
 #   providers = {
-#     aws = "aws.eu-west-2"
+#     aws = aws.eu-west-2
 #   }
 # }
 
 module "aws_deploy-integration-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.0"
   env               = "integration"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
@@ -57,24 +49,18 @@ module "aws_deploy-integration-eu-west-2" {
 
   spot_price    = "0.07"
   instance_type = "m4.large"
-  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+  ami_name      = "aeternity-ubuntu-18.04-v1616681631"
 
   additional_storage      = true
   additional_storage_size = 120
 
-  snapshot_filename = "mnesia_uat_v-1_latest.tgz"
-
-  aeternity = {
-    package = "${var.package}"
-  }
-
   providers = {
-    aws = "aws.eu-west-2"
+    aws = aws.eu-west-2
   }
 }
 
 module "aws_deploy-integration_sync-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.0"
   env               = "integration_sync"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
@@ -85,24 +71,18 @@ module "aws_deploy-integration_sync-eu-west-2" {
 
   spot_price    = "0.10"
   instance_type = "c5.large"
-  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+  ami_name      = "aeternity-ubuntu-18.04-v1616681631"
 
   additional_storage      = true
   additional_storage_size = 120
 
-  snapshot_filename = "empty"
-
-  aeternity = {
-    package = "${var.package}"
-  }
-
   providers = {
-    aws = "aws.eu-west-2"
+    aws = aws.eu-west-2
   }
 }
 
 module "aws_deploy-next-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.0"
   env               = "next"
   bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
@@ -116,24 +96,20 @@ module "aws_deploy-next-eu-west-2" {
 
   spot_price    = "0.07"
   instance_type = "m4.large"
-  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+  ami_name      = "aeternity-ubuntu-18.04-v1616681631"
 
   additional_storage      = true
   additional_storage_size = 120
 
-  aeternity = {
-    package = var.package
-  }
-
   providers = {
-    aws = "aws.eu-west-2"
+    aws = aws.eu-west-2
   }
 }
 
 resource "aws_route53_record" "next" {
-  zone_id = "${var.dns_zone}"
-  name    = "${var.next_domain}"
+  zone_id = var.dns_zone
+  name    = var.next_domain
   type    = "A"
-  records = "${module.aws_deploy-next-eu-west-2.static_node_ips}"
+  records = module.aws_deploy-next-eu-west-2.static_node_ips
   ttl     = 300
 }
--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "bootstrap_version" {
   default = "v2.12.2"
 }
 
-variable "package" {
-  default = "https://builds.aeternity.io/aeternity-latest-ubuntu-x86_64.tar.gz"
-}
-
 variable "dns_zone" {
   default = "ZSEEAAX46MKWZ"
 }
