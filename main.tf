37ebc906411924be93a1da00236ca8a77da67e3b
Dincho Todorov <dincho@users.noreply.github.com>
2023-10-10 15:29:59 +0300

    Destroy next environment

--- main.tf
+++ main.tf
@@ -102,45 +102,45 @@ module "aws_deploy-integration_sync-eu-west-2" {
   }
 }
 
-module "aws_deploy-next-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.0.0"
-  env               = "next"
+# module "aws_deploy-next-eu-west-2" {
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.0.0"
+#   env               = "next"
 
-  instance_type  = "m5.large"
-  instance_types = ["m6i.large", "m5d.large", "m5.large"]
-  ami_name       = "aeternity-ubuntu-18.04-v1653564902"
+#   instance_type  = "m5.large"
+#   instance_types = ["m6i.large", "m5d.large", "m5.large"]
+#   ami_name       = "aeternity-ubuntu-18.04-v1653564902"
 
-  static_nodes   = 1
-  spot_nodes_min = 2
-  spot_nodes_max = 2
+#   static_nodes   = 1
+#   spot_nodes_min = 2
+#   spot_nodes_max = 2
 
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 120
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 120
 
-  enable_internal_api = true
+#   enable_internal_api = true
 
-  tags = {
-    role  = "aenode"
-    env   = "next"
-  }
+#   tags = {
+#     role  = "aenode"
+#     env   = "next"
+#   }
 
-  config_tags = {
-    bootstrap_version = var.bootstrap_version
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    node_config       = "secret/aenode/config/next"
-  }
+#   config_tags = {
+#     bootstrap_version = var.bootstrap_version
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     node_config       = "secret/aenode/config/next"
+#   }
 
-  providers = {
-    aws = aws.eu-west-2
-  }
-}
+#   providers = {
+#     aws = aws.eu-west-2
+#   }
+# }
 
-resource "aws_route53_record" "next" {
-  zone_id = var.dns_zone
-  name    = var.next_domain
-  type    = "A"
-  records = module.aws_deploy-next-eu-west-2.static_node_ips
-  ttl     = 300
-}
+# resource "aws_route53_record" "next" {
+#   zone_id = var.dns_zone
+#   name    = var.next_domain
+#   type    = "A"
+#   records = module.aws_deploy-next-eu-west-2.static_node_ips
+#   ttl     = 300
+# }
