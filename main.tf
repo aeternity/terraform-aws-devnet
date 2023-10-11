6b3bc5a88895b6f6030f40a357ae316c3a979951
Dincho Todorov <dincho@users.noreply.github.com>
2023-10-11 11:04:50 +0300

    Cost tuning (#37)
    * Update to latest aenode module (3.0.0)
    * Destroy next environment
    * Destroy integration_sync environment

--- main.tf
+++ main.tf
@@ -37,83 +37,110 @@
 # }
 
 module "aws_deploy-integration-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.1"
-  env               = "integration"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
+  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.0.0"
+  env    = "integration"
 
   static_nodes   = 1
   spot_nodes_min = 2
   spot_nodes_max = 2
 
-  spot_price    = "0.08"
-  instance_type = "m5.large"
-  ami_name      = "aeternity-ubuntu-18.04-v1653564902"
+  instance_type  = "m5.large"
+  instance_types = ["m6i.large", "m5d.large", "m5.large"]
+  ami_name       = "aeternity-ubuntu-18.04-v1653564902"
 
   root_volume_size        = 24
   additional_storage      = true
   additional_storage_size = 240
 
-  providers = {
-    aws = aws.eu-west-2
+  tags = {
+    role = "aenode"
+    env  = "integration"
   }
-}
 
-module "aws_deploy-integration_sync-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.1"
-  env               = "integration_sync"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
-
-  static_nodes   = 3
-  spot_nodes_min = 0
-  spot_nodes_max = 0
-
-  spot_price    = "0.15"
-  instance_type = "c5.xlarge"
-  ami_name      = "aeternity-ubuntu-18.04-v1653564902"
-
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 240
+  config_tags = {
+    bootstrap_version = var.bootstrap_version
+    vault_role        = "ae-node"
+    vault_addr        = var.vault_addr
+    node_config       = "secret/aenode/config/integration"
+  }
 
   providers = {
     aws = aws.eu-west-2
   }
 }
 
-module "aws_deploy-next-eu-west-2" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.6.1"
-  env               = "next"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
+# module "aws_deploy-integration_sync-eu-west-2" {
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.0.0"
+#   env               = "integration_sync"
 
-  static_nodes   = 1
-  spot_nodes_min = 2
-  spot_nodes_max = 2
+#   static_nodes   = 3
+#   spot_nodes_min = 0
+#   spot_nodes_max = 0
 
-  enable_internal_api = true
+#   instance_type  = "c5.xlarge"
+#   instance_types = ["c6i.xlarge", "c5d.xlarge", "c6in.xlarge", "c5.xlarge"]
+#   ami_name       = "aeternity-ubuntu-18.04-v1653564902"
 
-  spot_price    = "0.08"
-  instance_type = "m5.large"
-  ami_name      = "aeternity-ubuntu-18.04-v1653564902"
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 240
 
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 120
+#   tags = {
+#     role  = "aenode"
+#     env   = "integration_sync"
+#   }
 
-  providers = {
-    aws = aws.eu-west-2
-  }
-}
+#   config_tags = {
+#     bootstrap_version = var.bootstrap_version
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     node_config       = "secret/aenode/config/integration_sync"
+#   }
 
-resource "aws_route53_record" "next" {
-  zone_id = var.dns_zone
-  name    = var.next_domain
-  type    = "A"
-  records = module.aws_deploy-next-eu-west-2.static_node_ips
-  ttl     = 300
-}
+#   providers = {
+#     aws = aws.eu-west-2
+#   }
+# }
+
+# module "aws_deploy-next-eu-west-2" {
+#   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.0.0"
+#   env               = "next"
+
+#   instance_type  = "m5.large"
+#   instance_types = ["m6i.large", "m5d.large", "m5.large"]
+#   ami_name       = "aeternity-ubuntu-18.04-v1653564902"
+
+#   static_nodes   = 1
+#   spot_nodes_min = 2
+#   spot_nodes_max = 2
+
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 120
+
+#   enable_internal_api = true
+
+#   tags = {
+#     role  = "aenode"
+#     env   = "next"
+#   }
+
+#   config_tags = {
+#     bootstrap_version = var.bootstrap_version
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     node_config       = "secret/aenode/config/next"
+#   }
+
+#   providers = {
+#     aws = aws.eu-west-2
+#   }
+# }
+
+# resource "aws_route53_record" "next" {
+#   zone_id = var.dns_zone
+#   name    = var.next_domain
+#   type    = "A"
+#   records = module.aws_deploy-next-eu-west-2.static_node_ips
+#   ttl     = 300
+# }
